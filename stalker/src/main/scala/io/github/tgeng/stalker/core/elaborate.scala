01c8cfeabe508a894f0de5800b66a0cc20739cad
Tianyu Geng <tgeng@google.com>
2020-05-03 17:08:36 -0700

    refactor

--- stalker/src/main/scala/io/github/tgeng/stalker/core/elaborate.scala
+++ stalker/src/main/scala/io/github/tgeng/stalker/core/elaborate.scala
@@ -15,60 +15,49 @@ import ClauseT._
 import UncheckedRhs._
 
 extension elaboration on (p: Problem) {
-  def elaborate(using Γ: Context)(using Σ: Signature)(using clauses: ArrayBuffer[Clause]) : Result[CaseTree] = {
-    p match {
-      // User input available
-      case (_P@((_E1, q̅1) |-> rhs1) :: _) ||| (f, q̅) ∷ _C => _E1.solve match {
-        // Done
-        case Right(σ) if q̅1.isEmpty => rhs1 match {
-          case UTerm(v) => {
-              val vσ = v.subst(σ)
-              (vσ ∷ _C).check match {
-              case Right(_) => {
-                clauses.append(CheckedClause(Γ.toClosedTelescope, q̅, vσ, _C))
-                Right(CTerm(vσ))
-              }
-              case Left(e) => Left(e)
+  def elaborate(using Γ: Context)(using Σ: Signature)(using clauses: ArrayBuffer[Clause]) : Result[CaseTree] = p match {
+    // Done
+    case ((_E1, Nil) |-> rhs1) :: _ ||| (f, q̅) ∷ _C if _E1.solve.isRight => _E1.solve match {
+      case Right(σ) => rhs1 match {
+        case UTerm(v) => {
+            val vσ = v.subst(σ)
+            (vσ ∷ _C).check match {
+            case Right(_) => {
+              clauses.append(CheckedClause(Γ.toClosedTelescope, q̅, vσ, _C))
+              Right(CTerm(vσ))
             }
-          }
-          case _ => typingError("False impossible case.")
-        }
-        case _ => _C match {
-          // Intro
-          case _F@WFunction(_A, _B) => for {
-            wA <- _A.whnf 
-            r <- withCtxExtendedBy(_F.argName ∷ wA) {
-              for {
-                wB <- _B.whnf
-                _Pmod <- _P(wA)
-                r <- (_Pmod ||| (f, q̅.map(_.raise(1)) :+ QPattern(PVar(0))) ∷ wB).elaborate
-              } yield CLam(r)
-            }
-          } yield r
-          // CoSplit
-          // The original paper has CoSplitEmpty. But it does not seem to be
-          // necessary if we just use empty user input for that.
-          case WRecord(qn, v̅) => for {
-            record <- Σ getRecord qn
-            fields <- record.getFields
-            fieldCaseTrees <- fields.foldLeft[Result[Map[String, CaseTree]]](Right(Map())){ (acc, field) =>
-              for {
-                m <- acc
-                _Pmod <- _P(field.name, fields.map(_.name).toSet)
-                wA <- field.ty.substHead(v̅ :+ TRedux(f, q̅.map(_.toElimination))).whnf
-                q <- (_Pmod ||| (f, q̅ :+ QProj(field.name)) ∷ wA).elaborate
-              } yield m ++ Map(field.name -> q)
-            }
-          } yield CRecord(fieldCaseTrees)
-          // Split
-          case _ => {
-            ???
+            case Left(e) => Left(e)
           }
         }
+        case _ => typingError("False impossible case.")
       }
-      // No user input. Try to find empty type in Γ
-      case Nil ||| (f, q̅) ∷ _C => ???
+      case Left(_) => throw AssertionError()
     }
+    // Intro
+    case _P ||| (f, q̅) ∷ (_F@WFunction(_A, _B)) => for {
+      wA <- _A.whnf 
+      r <- withCtxExtendedBy(_F.argName ∷ wA) {
+        for {
+          wB <- _B.whnf
+          _Pmod <- _P.shift(wA)
+          r <- (_Pmod ||| (f, q̅.map(_.raise(1)) :+ QPattern(PVar(0))) ∷ wB).elaborate
+        } yield CLam(r)
+      }
+    } yield r
+    // Cosplit
+    case _P ||| (f, q̅) ∷ (WRecord(qn, v̅)) => for {
+      record <- Σ getRecord qn
+      fields <- record.getFields
+      fieldCaseTrees <- fields.foldLeft[Result[Map[String, CaseTree]]](Right(Map())){ (acc, field) =>
+        for {
+          m <- acc
+          _Pmod <- _P.filter(field.name, fields.map(_.name).toSet)
+          wA <- field.ty.substHead(v̅ :+ TRedux(f, q̅.map(_.toElimination))).whnf
+          q <- (_Pmod ||| (f, q̅ :+ QProj(field.name)) ∷ wA).elaborate
+        } yield m ++ Map(field.name -> q)
+      }
+    } yield CRecord(fieldCaseTrees)
+    case _ => typingError("Elaboration failed.")
   }
 }
 
@@ -95,19 +84,19 @@ private def (_E: Set[(Term /? Pattern) ∷ Type]) solve(using Γ: Context)(using
   }
 }
 
-private def (_P: UserInput) apply(_A: Type): Result[UserInput] = _P match {
+private def (_P: UserInput) shift(_A: Type): Result[UserInput] = _P match {
   case Nil => Right(Nil)
   case ((_E, QPattern(p) :: q̅) |-> rhs) :: _P => for {
-    _Pmod <- _P(_A)
+    _Pmod <- _P.shift(_A)
   } yield ((_E.map{ case (w /? p) ∷ _B => (w.raise(1) /? p) ∷ _B } union Set((TWhnf(WVar(0, Nil)) /? p) ∷ _A) , q̅) |-> rhs) :: _Pmod
   case _ => typingError("Unexpected clause")
 }
 
-private def (_P: UserInput) apply(fieldName: String, allFieldNames: Set[String]): Result[UserInput] = _P match {
+private def (_P: UserInput) filter(fieldName: String, allFieldNames: Set[String]): Result[UserInput] = _P match {
   case Nil => Right(Nil)
   case ((_E, QProj(π) :: q̅) |-> rhs) :: _P => 
     if (allFieldNames.contains(π)) 
-      for _Pmod <- _P(fieldName, allFieldNames)
+      for _Pmod <- _P.filter(fieldName, allFieldNames)
       yield 
         if (fieldName == π) ((_E, q̅) |-> rhs) :: _Pmod
         else _Pmod
