507d7f33172a886281b9aade18b59af7e5d1156f
Tianyu Geng <tgeng@google.com>
2020-05-25 15:59:06 -0700

    simplify parser

--- stalker/src/main/scala/io/github/tgeng/stalker/core/fe/parser.scala
+++ stalker/src/main/scala/io/github/tgeng/stalker/core/fe/parser.scala
@@ -18,60 +18,57 @@ object parser {
   private val bodyPattern = s"[^${bodyInvalidChars}]"
   private val headPattern = s"""[^`'"0-9${bodyInvalidChars}]"""
   private val name = P { s"$headPattern$bodyPattern*".rp.satisfying(!Set("->", ":", "=", "_").contains(_)) }
-  private val level = P { "[0-9]+".rp.map(s => Right(FTLevel(Integer.parseInt(s)))) << "lv" }
+  private val level = P { "[0-9]+".rp.map(s => FTLevel(Integer.parseInt(s))) << "lv" }
 
-  private def con(using opt: ParsingOptions) : Parser[Result[FTerm]] = P {
+  private def con(using opt: ParsingOptions) : Parser[FTerm] = P {
     for {
       n <- name
       args <- '{' >> whitespaces >> (termImpl sepBy (whitespaces >> ',' << whitespaces)) << whitespaces << '}'
-    } yield args.liftMap(t => t).map(args => FTCon(n, args.toList))
+    } yield FTCon(n, args.toList)
   }
 
-  private def ref(using opt: ParsingOptions) : Parser[Result[FTerm]] = P {
+  private def ref(using opt: ParsingOptions) : Parser[FTerm] = P {
     (name sepBy1 '.').map (names => names.toList match {
-      case head :: names => Right(FTRedux(head, names, Nil))
+      case head :: names => FTRedux(head, names, Nil)
       case _ => throw IllegalStateException("sepBy1 must return non empty vector")
     })
   }
 
-  private def atom(using opt: ParsingOptions) : Parser[Result[FTerm]] = P {
+  private def atom(using opt: ParsingOptions) : Parser[FTerm] = P {
     '('.! >> termImpl(using opt.copy(appDelimiter = whitespaces)) << ')' | con | ref | level
   }
 
   private def proj = '.'.! >> name
 
-  private def elim(using opt: ParsingOptions) : Parser[Result[FElimination]] = P {
-    atom.map(t => t.map(FETerm(_))) | proj.map(p => Right(FEProj(p)))
+  private def elim(using opt: ParsingOptions) : Parser[FElimination] = P {
+    atom.map(FETerm(_)) | proj.map(p => FEProj(p))
   }
 
-  private def app(using opt: ParsingOptions) : Parser[Result[FTerm]] = P {
+  private def app(using opt: ParsingOptions) : Parser[FTerm] = P {
     lift(atom, (opt.appDelimiter >> elim).*).flatMap {
-      case (Right(t), elims) => if (elims.isEmpty) {
-        pure(Right(t))
+      case (t, elims) => if (elims.isEmpty) {
+        pure(t)
       } else {
-        val liftedElims : Result[Vector[FElimination]] = elims.liftMap(e => e)
-        (t, liftedElims) match {
-          case (FTRedux(head, name, es1), Right(es2)) => pure(Right(FTRedux(head, name, es1 ++ es2)))
-          case (FTFunction(_, _), Right(_)) => pure(typingError("Cannot apply to function type."))
-          case (_, Left(e)) => pure(Left(e))
-          case _ => throw IllegalStateException()
+        (t, elims) match {
+          case (FTRedux(head, name, es1), es2) => pure(FTRedux(head, name, es1 ++ es2))
+          case (FTFunction(_, _), _) => fail("Cannot apply to function type.")
+          case (FTLevel(_), _) => fail("Cannot apply to a level.")
+          case (FTCon(_, _), _) => fail("Cannot apply to a constructed value.")
         }
       }
-      case (Left(e), _) => pure(Left(e))
     }
   }
 
-  private def binding(using opt: ParsingOptions) : Parser[Result[FBinding]] = P {
-    '(' >> whitespaces >> lift(name << whitespaces, ':'.! >> whitespaces >> termImpl).map((x, ty) => ty.map(FBinding(x, _))) << whitespaces << ')' | 
-    app.map(_.map(t => FBinding("", t)))
+  private def binding(using opt: ParsingOptions) : Parser[FBinding] = P {
+    '(' >> whitespaces >> lift(name << whitespaces, ':'.! >> whitespaces >> termImpl).map((x, ty) => FBinding(x, ty)) << whitespaces << ')' | 
+    app.map(FBinding("", _))
   }
 
-  private def termImpl(using opt: ParsingOptions) : Parser[Result[FTerm]] = P {
+  private def termImpl(using opt: ParsingOptions) : Parser[FTerm] = P {
     for {
       bdn <- (binding << whitespaces << "->".! << whitespaces).?
       r <- bdn match {
-        case Some(Right(b)) => for t <- termImpl(using opt) yield t.map(FTFunction(b, _))
-        case Some(Left(e)) => pure(Left(e))
+        case Some(b) => for t <- termImpl(using opt) yield FTFunction(b, t)
         case None => app
       }
     } yield r
