dec9cab9e8ec3056ec41508b9af129d64efd0171
Tianyu Geng <tgeng@google.com>
2020-06-28 23:09:26 -0700

    add tiny test and stdlib

--- stalker/src/test/scala/io/github/tgeng/stalker/io/ModuleLoaderSpec.scala
+++ stalker/src/test/scala/io/github/tgeng/stalker/io/ModuleLoaderSpec.scala
@@ -1,8 +1,22 @@
 package io.github.tgeng.stalker.io
 
+import scala.language.implicitConversions
+import io.github.tgeng.common.eitherOps
 import io.github.tgeng.testing.UnitSpec
+import io.github.tgeng.stalker
+import stalker.common.QualifiedName.{given _, _}
 
 class ModuleLoaderSpec extends UnitSpec {
   val pathResolver = PathResolver.createTmp(Nil)
+  val moduleLoader = ModuleLoader(pathResolver)
 
+  "loading nat module should work" in {
+    val natModule = moduleLoader.loadModule("stalker.data.nat.base")
+    assert(natModule.!!!.commands.size > 0)
+    // Serialization does not work yet due to https://github.com/lampepfl/dotty/issues/9179
+
+    // val moduleLoader2 = ModuleLoader(pathResolver)
+    // val natModule2 = moduleLoader2.loadModule("stalker.data.nat.base")
+    // assert(natModule == natModule2)
+  }
 }
