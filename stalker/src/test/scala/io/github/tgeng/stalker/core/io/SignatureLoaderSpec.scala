a06734fef4109763a04e33cd50def3aefc30d2c1
Tianyu Geng <tgeng@google.com>
2020-07-04 15:05:42 -0700

    add a few test cases

--- stalker/src/test/scala/io/github/tgeng/stalker/core/io/SignatureLoaderSpec.scala
+++ stalker/src/test/scala/io/github/tgeng/stalker/core/io/SignatureLoaderSpec.scala
@@ -9,6 +9,9 @@ class SignatureLoaderSpec extends CoreSpec {
   withSignature("io.github.tgeng.stalker.core.io.basic") {
     "basic typing" in {
       t"unit" ∷ t"Unit"
+      t"MyLevel" ∷ t"Type 0lv"
+      t"myLmax 1lv 2lv" ~> ft"2lv"
+      t"Pair{0lv 1lv}" ∷ t"Pair 0lv Level"
     }
   }
 }
